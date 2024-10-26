731a5d429b7724b71108fa8b53fa1bc162426058
Habtnan <131915962+Habtnan@users.noreply.github.com>
2024-10-27 01:04:31 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Markdown Previewer
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/habtewold-minycheal/pen/GRVMObQ](https://codepen.io/habtewold-minycheal/pen/GRVMObQ).
+
---
+++ src/script.js
@@ -0,0 +1,38 @@
+
+import React from "https://esm.sh/react";
+import ReactDOM from "https://esm.sh/react-dom";
+import { marked } from "https://esm.sh/marked";
+
+
+const defaultMarkdown = `# Welcome to my Markdown Previewer!
+
+## This is a sub-heading!
+
+[Learn more about Markdown](https://www.markdownguide.org/)
+
+Inline code: \`const x = 10;\`
+
+\`\`\`
+// This is a code block
+function helloWorld() {
+  console.log("Hello, World!");
+}
+\`\`\`
+
+- List item 1
+- List item 2
+- List item 3
+
+> This is a blockquote
+
+![Image](https://via.placeholder.com/150)
+
+**This text is bold**`;
+
+document.getElementById('editor').value = defaultMarkdown;
+document.getElementById('preview').innerHTML = marked(defaultMarkdown);
+
+document.getElementById('editor').addEventListener('input', (event) => {
+  const markdownText = event.target.value;
+  document.getElementById('preview').innerHTML = marked(markdownText);
+});
